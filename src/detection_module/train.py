6702177ce87bae3280b0b0ea737458386905bdd1
nctrinh <nctrinh2705@gmail.com>
2025-11-11 09:58:19 +0700

    Finish training model yolo11n

--- src/detection_module/train.py
+++ src/detection_module/train.py
@@ -9,8 +9,6 @@ def main():
     results=model.val()
 
     success0 = model.export()
-    success1 = model.export(format="onnx", dynamic=True, int8=True)
-    success2 = model.export(format="engine", dynamic=True, int8=True)
 
 if __name__ == "__main__":
     main()
