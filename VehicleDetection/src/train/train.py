c6147f6c15bd19c225beb3ec926dab840a4ed2bd
NamSeko <trantiennam04@gmail.com>
2025-11-12 17:46:04 +0700

    Add model License Plate Detection

--- VehicleDetection/src/train/train.py
+++ VehicleDetection/src/train/train.py
@@ -1,3 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO()
+def main():
+    model = YOLO("yolov9s.pt")
+
+    results = model.train(
+        cfg="config.yaml"
+    )
+    results=model.val()
+
+if __name__ == "__main__":
+    main()
